f01edb4fcc41b02956d75c96b67818b71406ef53
samira2106 <154353012+samira2106@users.noreply.github.com>
2023-12-19 20:00:14 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+import streamlit as st
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+
+
+model_name = "deepset/roberta-base-squad2"
+
+# a) Get predictions
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+
+def main():
+  st.title("Question Answering with Transformers")
+
+  # Get user input
+  context = st.text_area("Context", height=200)
+  question = st.text_area("Question", height=100)
+
+  # Generate predictions
+  QA_input = {
+      'question': context,
+      'context': question
+  }
+  res = nlp(QA_input)
+
+  # Display predictions
+  st.subheader("Predictions")
+  st.write(res)
+
+if __name__ == "__main__":
+  main()
